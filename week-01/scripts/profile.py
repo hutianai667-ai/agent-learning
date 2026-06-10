fb5f754562e9c84cb46f0ee231d09692ad37e837
hutianai667-ai <hutianai667@gmail.com>
2026-06-10 22:03:34 +0800

    Expand profile f-string practice

--- week-01/scripts/profile.py
+++ week-01/scripts/profile.py
@@ -1,9 +1,10 @@
-def build_profile(name, goal):
-    return f"{name} is learning {goal}."
+def build_profile(name, goal, city):
+    return f"{name} from {city}is learning {goal}."
 
 
 name = input("What is your name? ")
+city = input("Where are you from")
 goal = input("What are you learning? ")
 
-message = build_profile(name, goal)
+message = build_profile(name, goal, city)
 print(message)
